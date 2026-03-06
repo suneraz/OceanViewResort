a6b3af134a33ac6da703c01cf1e7bb6213e531b5
suneraz <sunerar.2002@gmail.com>
2026-03-06 14:34:37 +0530

    Added more reservations

--- src/main/java/com/oceanview/util/DBConnection.java
+++ src/main/java/com/oceanview/util/DBConnection.java
@@ -8,7 +8,7 @@ public class DBConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/oceanview_resort";
     private static final String USER = "root";
-    private static final String PASSWORD = "root"; // 
+    private static final String PASSWORD = "solo"; // 
 
     private static DBConnection instance;
     private Connection connection;
